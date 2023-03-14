d4f09e89d64cf631a4d26b1a842d1b7b00f60dbf
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-03-14 21:07:21 +0100

    Update AddWordView.vue

--- client/src/views/AddWordView.vue
+++ client/src/views/AddWordView.vue
@@ -11,7 +11,25 @@
 export default {
     
 }
+<script>
+import WordContributionData from "components/WordAdmin/AddWord/data";
+import { makeStyles } from "@material-ui/core/styles";
+
+export default {
+  components: {
+    WordContributionData
+  },
+  setup() {
+    const classes = makeStyles(() => ({
+      root: {
+        margin: "3.2rem 0"
+      }
+    }))();
+
+    return {
+      classes
+    };
+  }
+};
 </script>
-<style lang="">
-    
-</style>
+
