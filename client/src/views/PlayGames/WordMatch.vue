17ddab19799e8481c3a387bf0f8c3a2cf37b6413
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-03-14 20:58:58 +0100

    WordMatch code

---
+++ client/src/views/PlayGames/WordMatch.vue
@@ -0,0 +1,19 @@
+<template>
+  <WordMatchGameData />
+</template>
+
+<script>
+import WordMatchGameData from 'components/PlayGames/WordMatch/data';
+import useCloseNavigation from 'hooks/useCloseNavigation';
+import useTitle from 'hooks/useTitle';
+
+export default {
+  mounted() {
+    useTitle('Game (Word matching game)');
+    useCloseNavigation();
+  },
+  components: {
+    WordMatchGameData
+  }
+};
+</script>
