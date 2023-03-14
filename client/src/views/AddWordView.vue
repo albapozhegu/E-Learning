ef096af241be20b9c3460d073863aba5792856aa
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-03-14 21:06:47 +0100

    Update AddWordView.vue

--- client/src/views/AddWordView.vue
+++ client/src/views/AddWordView.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        
+    <div class="container">
+    <div :class="classes.root">
+      <div class="ani-fade">
+        <WordContributionData />
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
