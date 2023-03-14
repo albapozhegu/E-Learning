cb9cbf2b607cda710e0926eb528c0d54a71c27ea
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-03-14 21:00:55 +0100

    FastGame code

---
+++ client/src/views/PlayGames/FastGame.vue
@@ -0,0 +1,19 @@
+<template>
+  <FastGameData />
+</template>
+
+<script>
+import FastGameData from 'components/PlayGames/FastGame/data';
+import useCloseNavigation from 'hooks/useCloseNavigation';
+import useTitle from 'hooks/useTitle';
+
+export default {
+  mounted() {
+    useTitle('Game hands faster than brain');
+    useCloseNavigation();
+  },
+  components: {
+    FastGameData
+  }
+};
+</script>
