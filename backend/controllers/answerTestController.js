347ba1bda2d34ef68489284271f7af53a75595fc
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-03-16 23:04:32 +0100

    Update answerTestController.js

--- backend/controllers/answerTestController.js
+++ backend/controllers/answerTestController.js
@@ -75,6 +75,6 @@ try {
   return res.status(200).json({ answers });
 } catch (error) {
   console.error("ERROR: ", error);
-  return res.status(503).json({ message: "Lỗi dịch vụ, thử lại sau" });
+  return res.status(503).json({ message: "Service error, try again later" });
 }
 };
