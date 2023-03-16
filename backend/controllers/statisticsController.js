d4956e3ab1a1667af2a604f3ed2c0552dc03be79
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-03-16 23:12:06 +0100

    Update statisticsController.js

--- backend/controllers/statisticsController.js
+++ backend/controllers/statisticsController.js
@@ -3,6 +3,7 @@ const Words = require("../models/wordModel");
 const Listens = require("../models/listeningModel");
 const Quizzes = require("../models/Quizzes/quizModel");
 const Grammars = require("../models/Grammar/grammarModel");
+
 exports.countUser = async (req, res) => {
   const count = await Users.countDocuments();
   return res.status(200).json({ count });
